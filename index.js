b1a070ba5c1e73f7d93552d3ac3d2ca0cb64d63c
ansar <ansarsaudagar40@gmail.com>
2024-07-11 21:10:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('hello');
+
+console.log('start');
+
+setTimeout(() => {
+    console.log('inside settimeout');
+}, 0);
+
+new Promise(function(resolve){
+    console.log('inside promise excutor');
+    resolve();
+}).then(function(){
+    console.log('Promise Resolved')
+});
+
+console.log('end');
---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const listner = (req, res)=> {
+    res.write('hello world');
+    res.end();
+}
+
+const server = http.createServer(listner);
+
+server.listen(4000,()=>{
+    console.log("server is running");
+});
