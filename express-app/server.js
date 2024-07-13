464665c782c0441c2c6a3d6ddd245119db27e5c6
vishal sontakke <sontakkev44@gmail.com>
2024-07-13 23:15:19 +0530

    middleware started

--- express-app/server.js
+++ express-app/server.js
@@ -2,6 +2,22 @@ const express = require ('express');
 
 const app = express();
 
+// const appLevelMiddleware = (req,res,next)=>{
+//     console.log("app in moddleware");
+//     next();
+// };
+// // then you have to pass that moddleware to the use 
+
+const authenticate = (req,res,next)=>{
+    const {api_key} = req.query;
+
+    if(api_key && api_key ==="vishal"){
+        return next();
+    } 
+    res.status(401).send('unauthenticated');
+}
+// app.use(appLevelMiddleware);
+
 app.use('/public',express.static( __dirname+'/images'));
 
 app.get('/',(req,res)=>{
@@ -12,10 +28,10 @@ app.get('/about',(req,res)=>{
     res.send('Express About!');
 });
 
-app.get('/contact',(req,res)=>{
-    console.log(req.query);
-    const {name, id} = req.query;
-    res.send('Express contact!'+id);
+app.get('/contact',authenticate,(req,res)=>{
+    // console.log(req.query);
+    // const {name, id} = req.query;
+    res.send('Express contact!');
 });
 
 app.get('/users/:id',(req,res)=>{
